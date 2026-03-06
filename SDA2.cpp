ea5aa42148fa91970ea1386b92059d21947e7f3f
pipetka-lang <stanislav.vasilisin@isa.utm.md>
2026-03-06 14:12:10 +0200

    SDA2.cpp

--- SDA2.cpp
+++ SDA2.cpp
@@ -17,38 +17,43 @@ typedef struct {
 
 void initList(RecordList *list, int capacity) {
     list->records = (MedicalRecord*)malloc(capacity * sizeof(MedicalRecord));
+    if (!list->records) {
+        fprintf(stderr, "Oshibka: ne udalos' vydelit' pamyat'\n");
+        exit(1);
+    }
     list->size = 0;
     list->capacity = capacity;
 }
 
 void addRecord(RecordList *list, MedicalRecord record) {
     if (list->size >= list->capacity) {
-        int newCapacity = list->capacity + 5;
-        MedicalRecord *newRecords = (MedicalRecord*)malloc(newCapacity * sizeof(MedicalRecord));
-        for (int i = 0; i < list->size; i++) {
-            newRecords[i] = list->records[i];
-        }
-        free(list->records);
-        list->records = newRecords;
-        list->capacity = newCapacity;
+        printf("Dostignut maksimum zapisey (%d).\n", list->capacity);
+        return;
     }
     list->records[list->size++] = record;
 }
 
-
 void removeLastRecord(RecordList *list) {
     if (list->size > 0) {
         list->size--;
+        printf("Poslednyaya zapis' udalena.\n");
+    } else {
+        printf("Spisok pust.\n");
     }
 }
 
-void printRecords(RecordList *list) {
+void printRecords(const RecordList *list) {
+    if (list->size == 0) {
+        printf("Spisok pust.\n");
+        return;
+    }
     for (int i = 0; i < list->size; i++) {
-        printf("FIO: %s\nDate: %s\nDiagnosis: %s\nTreatment: %s\n\n",
-               list->records[i].fio,
-               list->records[i].date,
-               list->records[i].diagnosis,
-               list->records[i].treatment);
+        printf("Zapis' %d:\n", i + 1);
+        printf("  FIO: %s\n", list->records[i].fio);
+        printf("  Data: %s\n", list->records[i].date);
+        printf("  Diagnoz: %s\n", list->records[i].diagnosis);
+        printf("  Lechenie: %s\n", list->records[i].treatment);
+        printf("-----------------------------\n");
     }
 }
 
@@ -57,24 +62,89 @@ void clearList(RecordList *list) {
     list->records = NULL;
     list->size = 0;
     list->capacity = 0;
+    printf("Pamyat' ochishchena. Vyhod iz programmy.\n");
+}
+
+MedicalRecord createRecord() {
+    MedicalRecord r;
+
+    printf("Vvedite FIO: ");
+    scanf(" %49[^\n]", r.fio);
+
+    printf("Vvedite datu: ");
+    scanf(" %19[^\n]", r.date);
+
+    printf("Vvedite diagnoz: ");
+    scanf(" %99[^\n]", r.diagnosis);
+
+    printf("Vvedite lechenie: ");
+    scanf(" %99[^\n]", r.treatment);
+
+    return r;
+}
+
+void searchRecords(const RecordList *list, const char *query) {
+    int found = 0;
+    for (int i = 0; i < list->size; i++) {
+        if (strstr(list->records[i].fio, query) ||
+            strstr(list->records[i].date, query) ||
+            strstr(list->records[i].diagnosis, query) ||
+            strstr(list->records[i].treatment, query)) {
+
+            printf("Naydena zapis' %d:\n", i + 1);
+            printf("  FIO: %s\n", list->records[i].fio);
+            printf("  Data: %s\n", list->records[i].date);
+            printf("  Diagnoz: %s\n", list->records[i].diagnosis);
+            printf("  Lechenie: %s\n", list->records[i].treatment);
+            printf("-----------------------------\n");
+            found = 1;
+        }
+    }
+    if (!found) {
+        printf("Po zaprosu '%s' nichego ne naydeno.\n", query);
+    }
 }
 
 int main() {
     RecordList list;
-    initList(&list, 2);
+    initList(&list, 3);
 
-    MedicalRecord r1 = {"Ivanov I.I.", "20.02.2026", "ORVI", "Postel'nyy rezhim"};
-    MedicalRecord r2 = {"Petrov P.P.", "21.02.2026", "Gripp", "Antivirusnye preparaty"};
+    int command;
+    char query[100];
 
-    addRecord(&list, r1);
-    addRecord(&list, r2);
+    while (1) {
+        printf("\nMenu:\n");
+        printf("1 - dobavit' zapis\n");
+        printf("2 - pokazat' vse zapisi\n");
+        printf("3 - udalit' poslednyuyu zapis\n");
+        printf("4 - poiski po zaprosu\n");
+        printf("5 - vyhod\n");
+        printf("Vash vybor: ");
 
-    printRecords(&list);
+        if (scanf("%d", &command) != 1) {
+            while (getchar() != '\n');
+            printf("Oshibka vvoda. Poprobuyte snova.\n");
+            continue;
+        }
 
-    removeLastRecord(&list);
-    printf("Posle udaleniya:\n");
-    printRecords(&list);
+        if (command == 1) {
+            MedicalRecord r = createRecord();
+            addRecord(&list, r);
+        } else if (command == 2) {
+            printRecords(&list);
+        } else if (command == 3) {
+            removeLastRecord(&list);
+        } else if (command == 4) {
+            printf("Vvedite stroku dlya poiska: ");
+            scanf(" %99[^\n]", query);
+            searchRecords(&list, query);
+        } else if (command == 5) {
+            clearList(&list);
+            break;
+        } else {
+            printf("Neizvestnaya komanda.\n");
+        }
+    }
 
-    clearList(&list);
     return 0;
 }
